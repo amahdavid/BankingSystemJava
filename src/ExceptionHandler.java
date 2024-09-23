48833a7b8c4db906fda6f5a2d198a5514147d025
amahdavid <amahdavid34@gmail.com>
2024-09-23 15:43:26 -0700

    Removed a comment

--- src/ExceptionHandler.java
+++ src/ExceptionHandler.java
@@ -1,5 +1,5 @@
 public class ExceptionHandler extends Exception {
     public ExceptionHandler(String message) {
-        super(message);  // Calls the constructor of the Exception class with the message
+        super(message);
     }
 }
